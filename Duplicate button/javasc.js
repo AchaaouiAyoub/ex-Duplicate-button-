c435448fa82d98c4804ec04689690c97d5a9f996
Achaaoui ayoub <118342687+AchaaouiAyoub@users.noreply.github.com>
2023-03-08 09:21:44 +0100

    Add files via upload

---
+++ Duplicate button/javasc.js
@@ -0,0 +1,6 @@
+
+function duplicate() {
+    var nouvelleDivision = document.createElement("button");
+    nouvelleDivision.setAttribute("id", "ayoub");
+    document.body.appendChild(nouvelleDivision);
+}
